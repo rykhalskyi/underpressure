26dce54b2248c17443c9767c636d72d53393d479
Iaroslav Rykhalskyi <rikhalskiy@gmail.com>
2026-04-28 21:22:40 +0200

    Add privacy policy to Settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.otakeessen.underpressure"
         minSdk = 29
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.6.7-alpha"
+        versionCode = 12
+        versionName = "1.6.9-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
