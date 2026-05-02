2c91d9b2f4484cd3c94a9c806120f5d6e1ad1503
Iaroslav Rykhalskyi <rikhalskiy@gmail.com>
2026-05-02 19:46:07 +0200

    add migrations

--- app/src/main/java/com/otakeessen/underpressure/data/local/database/AppDatabase.kt
+++ app/src/main/java/com/otakeessen/underpressure/data/local/database/AppDatabase.kt
@@ -5,6 +5,8 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.otakeessen.underpressure.data.local.converters.Converters
 import com.otakeessen.underpressure.data.local.dao.AppSettingsDao
 import com.otakeessen.underpressure.data.local.dao.MeasurementDao
@@ -31,6 +33,24 @@ abstract class AppDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: AppDatabase? = null
 
+        private val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(db: SupportSQLiteDatabase) {
+                // No changes in schema between 1 and 2
+            }
+        }
+
+        private val MIGRATION_2_3 = object : Migration(2, 3) {
+            override fun migrate(db: SupportSQLiteDatabase) {
+                db.execSQL("ALTER TABLE app_settings ADD COLUMN lastOnboardedVersion TEXT")
+            }
+        }
+
+        private val MIGRATION_3_4 = object : Migration(3, 4) {
+            override fun migrate(db: SupportSQLiteDatabase) {
+                db.execSQL("ALTER TABLE app_settings ADD COLUMN slotModifiedFlags TEXT NOT NULL DEFAULT 'false,false,false,false'")
+            }
+        }
+
         fun getDatabase(context: Context): AppDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
@@ -38,7 +58,7 @@ abstract class AppDatabase : RoomDatabase() {
                     AppDatabase::class.java,
                     "under_pressure_database"
                 )
-                .fallbackToDestructiveMigration()
+                .addMigrations(MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4)
                 .build()
                 INSTANCE = instance
                 instance
