5c1a11c185b1a30188c3a5d36622180c1653a643
Iaroslav Rykhalskyi <rikhalskiy@gmail.com>
2026-05-09 20:30:25 +0200

    increase version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.otakeessen.underpressure"
         minSdk = 29
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.6.17 -alpha"
+        versionCode = 21
+        versionName = "1.6.18 -alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
